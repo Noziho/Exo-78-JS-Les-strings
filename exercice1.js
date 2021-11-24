eeb47ee1d857536a1b4957af5312783a40c7f202
Noah2211 <noah.decroix3@gmail.com>
2021-11-24 16:41:18 +0100

    exercice almost done

--- exercice1.js
+++ exercice1.js
@@ -1,14 +1,23 @@
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-
+let newText = monTexte.toUpperCase();
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+console.log(monTexte.length);
 
-
+console.log(newText.includes('a'));
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+document.getElementById("position").innerText = monTexte.indexOf("i");
 
+document.getElementById("chaineMaj").innerText = newText;
 
+for (let i = 0; i < newText.length; i++) {
+    if (newText[i] === "a") {
+        alert("lettre a trouvé");
+        i++;
+    }
+
+}
 
 
 
